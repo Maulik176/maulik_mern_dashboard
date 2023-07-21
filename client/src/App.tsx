78599ad7a8039278032d34c939658f6796bdc502
Maulik Ranadive <maulik.r1@ahduni.edu.in>
2023-07-21 16:09:23 +0530

    server backend added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -60,7 +60,7 @@ function App() {
 
             if (profileObj) {
                 const response = await fetch(
-                    "http://localhost:8080/api/v1/users",
+                    "https://maulik-mern-dashboard.onrender.com/",
                     {
                         method: "POST",
                         headers: { "Content-Type": "application/json" },
@@ -131,7 +131,7 @@ function App() {
             <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
             <RefineSnackbarProvider>
                 <Refine
-                    dataProvider={dataProvider("http://localhost:8080/api/v1")}
+                    dataProvider={dataProvider("https://maulik-mern-dashboard.onrender.com/")}
                     notificationProvider={notificationProvider}
                     ReadyPage={ReadyPage}
                     catchAll={<ErrorComponent />}
