747c25d0044243f85cc483c3a0b6bbae62bb0fba
Maulik Ranadive <maulik.r1@ahduni.edu.in>
2023-07-21 16:27:28 +0530

    packages added

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import propertyRouter from "./routes/property.routes.js";
 dotenv.config();
 
 const app = express();
+app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
